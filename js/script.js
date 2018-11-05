4608fda0948dab616a049b754d1b4142a719c2d1
Layla Echchaouy <lechchaouy@aclanyc.com>
2018-11-05 23:07:51 +0000

    starting out rps

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+Math.floor((Math.random() * 3) + 1);
+var scissors= 1
+var rock= 2
+var paper = 3
 
-
+$("#shoot").click(function() {
+    var user_input = $("#input").val();
+    $("#userChoice").text(user_input);
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
